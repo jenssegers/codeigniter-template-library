f052b8deaf340b35f87d2efdb13b0d0cb1acded8
Jens Segers <segers.jens@gmail.com>
2012-01-20 18:01:38 +0100

    Wrong view file

--- application/views/widgets/hero.php
+++ application/views/widgets/hero.php
@@ -1,12 +1,5 @@
-<?php
-
-class hero_widget extends Widget {
-
-	public function display($args) {		
-		$data = array("title"=>$args["title"], 
-					  "text"=>"Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.");
-		
-		$this->load->view("widgets/hero", $data);
-	}
-	
-}
+<div class="hero-unit">
+	<h1><?php echo $title; ?></h1>
+	<p><?php echo $text; ?></p>
+	<p><a class="btn primary large">Learn more &raquo;</a></p>
+</div>
